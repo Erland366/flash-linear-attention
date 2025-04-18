b20b7de635e26dfeb6f2247b6214fad5996c6a27
zaydzuhri <zaydzuhri@gmail.com>
2025-04-18 14:16:15 +0400

    [Softpick] Add softpick attention implementations

--- fla/ops/attn/__init__.py
+++ fla/ops/attn/__init__.py
@@ -2,12 +2,16 @@
 
 from .parallel import parallel_attn
 from .parallel_rectified import parallel_rectified_attn
+from .parallel_softpick import parallel_softpick_attn
 from .naive import naive_attn
 from .naive_rectified import naive_rectified_attn
+from .naive_softpick import naive_softpick_attn
 
 __all__ = [
     'parallel_attn',
     'parallel_rectified_attn',
+    'parallel_softpick_attn',
     'naive_attn',
     'naive_rectified_attn',
+    'naive_softpick_attn',
 ]
